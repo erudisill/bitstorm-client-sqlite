2d2b35c4b72dbfdd194ec64d2e88bb6ae7e84e2d
Eric Rudisill <erudisill@cphandheld.com>
2014-12-29 16:07:18 -0600

    Inital db functionality - insert

---
+++ BitStormDb.py
@@ -0,0 +1,36 @@
+'''
+Created on Dec 29, 2014
+
+@author: ericrudisill
+'''
+import sqlite3
+
+class BitStormDb(object):
+
+    def __init__(self):
+        self.db = sqlite3.connect('./bitstorm.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+        self.initDb()
+
+    def initDb(self):
+        c = self.db.cursor()
+        
+        # Create records table
+        c.execute("SELECT * FROM sqlite_master WHERE name ='records' and type='table';")
+        result = c.fetchone()
+        if result == None:
+            self.db.execute('''
+                            CREATE TABLE records(ts timestamp, mac varchar, rssi int, temp int, batt int, cs int)
+                            ''')
+            self.db.commit()
+        c.close()
+
+    def close(self):
+        self.db.close()
+        
+    def insertRecord(self, record):
+        c = self.db.cursor()
+        c.execute("insert into records values(?,?,?,?,?,?)", \
+                  (record.ts, record.mac, record.rssi, record. temp, \
+                   record.batt, record.cs))
+        self.db.commit()
+        c.close()
--- main.py
+++ main.py
@@ -4,23 +4,30 @@ Created on Dec 29, 2014
 @author: ericrudisill
 '''
 import socket
-from binascii import hexlify
 from MessageAsciiFactory import MessageAsciiFactory
+from BitStormDb import BitStormDb
 
 HOST, PORT = "localhost", 1337
 
 def client(ip, port):
+    db = BitStormDb()
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
     maf = MessageAsciiFactory()
-    while True:
-        response = sock.recv(1024)
-        if not response: 
-            break
-        messages = maf.receive(response)
-        for m in messages:
-            print '[RCV] ' + str(m)
-    sock.close()
+    try:
+        while True:
+            response = sock.recv(1024)
+            if not response: 
+                break
+            records = maf.receive(response)
+            for r in records:
+                print '[RCV] ' + str(r)
+                db.insertRecord(r)
+    except Exception:
+        pass
+    finally:
+        db.close()
+        sock.close()
     
     
 if __name__ == '__main__':
